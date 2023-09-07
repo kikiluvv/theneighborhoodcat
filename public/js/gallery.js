6f31e7e000985d1732f33251751203a54bdfa1aa
kikiluvv <jrwenis@gmail.com>
2023-09-07 16:06:30 -0700

    final v1

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,39 +1,23 @@
-
-// JavaScript code to show/hide the scroll-to-top button and handle scrolling
-
-function toggleScrollButton() {
-    var button = document.getElementById('up-container');
-    if (window.scrollY > 200) {
-        button.classList.add('arrow-show'); // Add the "show" class
-    } else {
-        button.classList.remove('arrow-show'); // Remove the "show" class
-    }
-}
-
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
-
-window.addEventListener('scroll', toggleScrollButton);
-document.getElementById('up-container').addEventListener('click', scrollToTop);
-
-// Initial check for the scroll position to hide/show the button
-toggleScrollButton();
+//----------------------------------------------------------------------------------------------------//
 
 
 
-
-
+// Here you can add new sheets to the array, 
+// Put the images into the sheets folder and
+// call the path of the image here.
 var imageArray = [
     "/public/assets/gallery/sheets/1.jpg",
-    "/public/assets/gallery/sheets/2.jpg",
-    "/public/assets/gallery/sheets/3.png"
+    "/public/assets/gallery/sheets/2.jpg"
 ];
 
 var linkUrl = "https://form.jotform.com/232376224013042"; // Jotform URL
+
+
+
+
+//----------------------------------------------------------------------------------------------------//
+
+
 
 var imageGallery = document.getElementById("imageGallery");
 
@@ -47,9 +31,34 @@ imageArray.forEach(function (imageUrl) {
 
     var img = document.createElement("img");
     img.src = imageUrl;
+    img.className = 'image';
 
     anchor.appendChild(img);
     imageContainer.appendChild(anchor);
     imageGallery.appendChild(imageContainer);
 });
+
+
+
+function toggleScrollButton() {
+    var button = document.getElementById('up-container');
+    if (window.scrollY > 200) {
+        button.classList.add('arrow-show'); 
+    } else {
+        button.classList.remove('arrow-show'); 
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.addEventListener('scroll', toggleScrollButton);
+document.getElementById('up-container').addEventListener('click', scrollToTop);
+
+
+toggleScrollButton();
 
