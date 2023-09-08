17f269865c8c1913ab5150b4e08ef240120fe377
kikiluvv <jrwenis@gmail.com>
2023-09-08 12:48:09 -0700

    silver sparkles sista

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,8 +4,29 @@ const menu = document.querySelector(".mobile-nav-container");
 closeButton.addEventListener("click", () => {
   // Toggle the class on the menu element
   menu.classList.toggle("menu-open");
-  
+
   // Toggle the class on the burger icon
   closeButton.classList.toggle("ri-close-line");
   closeButton.classList.toggle("ri-menu-line");
+});
+
+
+// Get all buttons with the 'btn' class
+const buttons = document.querySelectorAll(".btn");
+
+// Add click event listeners to each button
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    // Get the value of the 'data-redirect' attribute
+    const redirectUrl = button.getAttribute("data-redirect");
+
+    // Check if a redirect URL is specified
+    if (redirectUrl) {
+      // Add a 500ms (half-second) delay before the redirect
+      setTimeout(function () {
+        // Redirect to the desired URL
+        window.location.href = redirectUrl;
+      }, 500); // 500 milliseconds (half-second)
+    }
+  });
 });
